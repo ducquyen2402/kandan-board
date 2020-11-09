a38bb55a01cc825823b5889fcecfa24810dec323
Quyen <ducquyen.ptit@gmail.com>
2020-11-09 13:55:10 +0700

    9/11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  todo = [
-    'Task 4',
-    'Task 5',
-    'Task 6'
+export class AppComponent{
+  taskList: Task[] = 
+  [
+    {
+      name: "TODO",
+      tasks: ["task1","task2"]
+    },
+    {
+      name: "DOING",
+      tasks: ["task3"]
+    },
+    {
+      name: "DONE",
+      tasks: ["task4"]
+    }
   ];
 
-  doing = [
-    'Task 3'
-  ];
-
-  done = [
-    'Task 1',
-    'Task 2'
-  ];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -30,5 +33,18 @@ export class AppComponent {
                         event.previousIndex,
                         event.currentIndex);
     }
+  }
+
+  addTask(input: HTMLInputElement, index: number){
+    if (input.value !== '') {
+      this.taskList[index].tasks.push(input.value);
+      input.value = '';
+    } else {
+      alert ("Task name is required");
+    }
+  }
+
+  removeTaskBlock(index: number) {
+    this.taskList.splice(index, 1);
   }
 }
